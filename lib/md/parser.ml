94dece32a27a08beea3ea2115c71cea868448d42
chiyoku <chiyokuoni@gmail.com>
2020-12-21 11:18:16 -0300

    :art: | Renamed markdown to md

---
+++ lib/md/parser.ml
@@ -0,0 +1,214 @@
+open Scanner
+
+type expr = 
+  | Bold of expr list
+  | Underline of expr list
+  | Italic of expr list
+  | Title of expr list * int
+  | Codeline of expr list
+  | Item of expr list
+  | Codeblock of string
+  | Text of string
+  | Linebreak
+  [@@deriving show]
+
+type error = 
+  | SyntaxError of token
+  | NotImplemented of token
+  [@@deriving show]
+
+(* Just to make it faster *)
+let next ctx = scan_token ctx
+
+(* 
+   it returns the token that it matched and advances to the next step in the Scanner 
+   if it not matches the pattern it will returns an Error
+*)
+
+let eat_pat ctx pat =
+  let token, new_ctx = next ctx in
+  if (pat token.token_type) then
+    Ok (token, new_ctx)
+  else
+    Error (SyntaxError token)
+
+(* eats a single token with one variant *)
+let eat ctx kind = eat_pat ctx (fun x -> x = kind)
+
+let eat_depth ctx pat = 
+  match eat_pat ctx pat with 
+    | Ok(_, ctx) ->
+      let rec loop_depth ctx =
+        match eat_pat ctx pat with 
+          | Ok (_, ctx) -> 
+            let depth, ctx = (loop_depth ctx) in
+            (1 + depth, ctx)   
+          | _ -> 
+            (1,ctx) in
+      Ok (loop_depth ctx)
+    | Error err -> Error err 
+
+let rec eat_exact_depth ctx pat count = 
+  match eat_pat ctx pat with 
+    | Ok(_, ctx) ->
+      if count > 1 then
+        eat_exact_depth ctx pat (count-1)
+      else
+        Ok(ctx)
+    | Error err -> 
+      Error err 
+    
+(* Transforms a token in string again *)
+let transform_to_text ctx =
+  let (tkn, new_ctx) = next ctx in
+    match tkn.token_type with 
+      | SHARP -> Ok "#", new_ctx
+      | MINUS -> Ok "-", new_ctx
+      | TEXT str -> Ok str, new_ctx
+      | UNDERSCORE -> Ok "_", new_ctx
+      | ASTERISK -> Ok "*", new_ctx
+      | ACUDE -> Ok "`", new_ctx
+      | LINEBREAK -> Ok "\n", new_ctx
+      | _ -> Error (SyntaxError tkn), ctx
+
+(* Join all the tokens after it as text *)
+let rec join_text_until_pat ctx pat = 
+  let tkn, _ = next ctx in
+  let matched, new_ctx = pat ctx in 
+  if (not matched) && tkn.token_type != EOF then
+    let text, next_ctx = transform_to_text new_ctx in
+    match text with 
+      | Error x -> Error x 
+      | Ok text -> 
+        match join_text_until_pat next_ctx pat with
+          | Ok(other, next_ctx) -> 
+            Ok (text^other, next_ctx)
+          | err -> err
+  else
+    Ok("", new_ctx)
+
+let rec parse_entry ctx =
+    let (tkn, new_ctx) = next ctx in
+      match tkn.token_type with 
+        | SHARP -> parse_title ctx
+        | ASTERISK -> parse_bold ctx
+        | ACUDE -> parse_code ctx
+        | MINUS -> parse_list ctx
+        | UNDERSCORE -> parse_underscore ctx
+        | LINEBREAK ->  Ok(Linebreak, new_ctx)
+        | TEXT str -> Ok(Text(str), new_ctx)
+        | _ -> Error (SyntaxError tkn)
+
+and group_until_pat ctx pat =
+  let tkn, _ = next ctx in
+  if (not (pat tkn.token_type)) then
+    match parse_entry ctx with
+      | Ok (ast, new_ctx) ->
+        let list, new_ctx = group_until_pat new_ctx pat in
+        ast::list, new_ctx
+      | _ -> 
+        [], ctx
+  else
+    [], ctx
+
+and parse_title ctx = 
+    let pat = (function SHARP -> true | _ -> false) in
+    match eat_depth ctx pat with
+      | Error err -> Error err  
+      | Ok(depth, ctx) ->
+        let pat = (function LINEBREAK | EOF -> true | _ -> false) in
+        let expr, ctx = (group_until_pat ctx pat) in
+        match eat_pat ctx pat with
+          | Ok(_, ctx) ->
+             Ok (Title (expr, depth), ctx)
+          | Error err -> Error err
+          
+and parse_bold ctx = 
+  let pat = (function ASTERISK -> true | _ -> false) in
+    match eat_depth ctx pat with
+      | Error err -> Error err  
+      | Ok(depth, ctx) ->
+        let pat = (function ASTERISK | EOF -> true | _ -> false) in
+        let expr, ctx = (group_until_pat ctx pat) in
+        match eat_exact_depth ctx (function ASTERISK -> true | _ -> false) depth with
+        | Error err -> Error err
+        | Ok(ctx) -> 
+          match depth with
+            | 1 -> Ok (Italic expr, ctx)
+            | 2 -> Ok (Bold expr, ctx) 
+            | _ -> Ok (Italic [Bold expr], ctx) 
+
+and parse_code ctx = 
+  let pat = (function ACUDE -> true | _ -> false) in
+    match eat_depth ctx pat with
+      | Error err -> Error err 
+      | Ok(depth, ctx) ->
+        if depth >= 3 then
+          let pat ctx = match eat_exact_depth ctx (function ACUDE | EOF -> true | _ -> false) depth with
+            | Error _ -> false, ctx
+            | Ok(ctx) -> true, ctx in
+          match join_text_until_pat ctx pat with
+            | Error err -> Error err
+            | Ok(text, ctx) ->
+              Ok (Codeblock text, ctx) 
+        else
+          let pat = (function ACUDE | EOF -> true | _ -> false) in
+          let expr, ctx = (group_until_pat ctx pat) in
+          match eat_exact_depth ctx (function ACUDE -> true | _ -> false) depth with
+          | Error err -> Error err
+          | Ok(ctx) -> Ok (Codeline expr, ctx) 
+
+and parse_list ctx = 
+  let pat = (function ACUDE -> true | _ -> false) in
+  match eat_depth ctx pat with
+    | Error err -> Error err  
+    | Ok(_depth, ctx) ->
+      let pat = (function LINEBREAK | EOF -> true | _ -> false) in
+      let expr, ctx = (group_until_pat ctx pat) in
+      match eat_pat ctx pat with
+        | Ok(_, ctx) -> Ok (Item expr, ctx)
+        | Error err -> Error err
+and parse_underscore ctx = 
+  let pat = (function UNDERSCORE -> true | _ -> false) in
+    match eat_depth ctx pat with
+      | Error err -> Error err  
+      | Ok(depth, ctx) ->
+        let pat = (function UNDERSCORE | EOF -> true | _ -> false) in
+        let expr, ctx = (group_until_pat ctx pat) in
+        match eat_exact_depth ctx (function UNDERSCORE -> true | _ -> false) depth with
+        | Error err -> Error err
+        | Ok(ctx) -> 
+          match depth with
+            (* If i want to add something here in the future.... *)
+            | _ -> Ok (Underline expr, ctx)
+
+let parse_info ctx =
+  let pat_simple = (function MINUS | EOF -> true | _ -> false) in
+  let pat_fun ctx = match eat_exact_depth ctx pat_simple 3 with
+    | Error _ -> false, ctx
+    | Ok(ctx) -> true, ctx in
+  match join_text_until_pat ctx pat_fun with
+    | Error err -> Error err
+    | Ok(text, ctx) ->
+      match eat_exact_depth ctx pat_simple 3 with
+      | Error err -> Error err
+      | Ok(ctx) -> Ok(text, ctx)
+
+(* Parses a sequence of expressions *)
+let rec parse_compound ctx = 
+  let tkn, _ = next ctx in
+    if tkn.token_type != EOF then
+      match parse_entry ctx with
+        | Error err -> Error err
+        | Ok (expression, new_ctx) -> 
+          match parse_compound new_ctx with
+            | Ok(expr) -> Ok (expression::expr)
+            | Error err -> Error err
+    else
+      Ok([])
+(* Returns a group of parsed expressions *)
+let parse text = 
+  let res = Scanner.new_context text |> parse_compound in
+  match res with
+    | Error err -> Error err
+    | Ok x -> Ok(x)
---
+++ lib/md/scanner.ml
@@ -0,0 +1,91 @@
+(** Scans a token from a string and returns a token from it. *)
+
+type token_type =
+  | SHARP 
+  | UNDERSCORE
+  | ASTERISK
+  | MINUS
+  | TEXT of string
+  | ACUDE
+  | EOF
+  | LINEBREAK
+  [@@deriving show]
+
+type token = {
+    line: int;
+    token_type: token_type;
+} 
+[@@deriving show]
+
+(* This is used to locate where it'll read next *)
+type scanner_context = {
+  source: string;
+  start: int;
+  current: int;
+  line: int;
+}
+
+let advance context = 
+  {context with current = context.current + 1}
+
+let advance_line context = 
+  {context with line = context.line + 1}
+
+let current_char context = 
+  if context.current < String.length context.source then
+    Some (context.source.[context.current])
+  else
+    None
+
+let next_char context = 
+  if context.current+1 < String.length context.source then
+    Some (context.source.[context.current+1])
+  else
+    None
+    
+let new_context str = 
+  { source = str; start = 0; current = -1; line = 1; }
+
+(* Creates a token with literal meaning *)
+let non_literal_tkn kind ctx = 
+  { line = ctx.line; token_type = kind; }, ctx
+
+(* converts char to token *)
+let escaped_tkn chr ctx = 
+  {line = ctx.line; token_type = TEXT(Core.Char.to_string chr); }, ctx
+
+let set_start ctx = 
+  { ctx with start = ctx.current }
+
+(* returns a token with text inside it *)
+let literal text line = 
+  { line = line; token_type = TEXT(text)}
+
+(*  creates a text literal by recursion *)
+let rec add_text_literal context =
+  let matching = function Some('#' | '_' | '*' | '-' | '$' | '`' | '\n' | '\\') | None -> true | _ -> false in
+  if (not (matching (current_char context))) then
+      add_text_literal (advance context)
+  else
+    let len = context.current - context.start in
+    let text = (String.sub context.source context.start len) in
+    (literal text context.line, {context with current = context.current - 1})
+
+let scan_token context =
+  let advanced_context = advance context in
+      match (current_char advanced_context) with
+      | Some '#' -> non_literal_tkn SHARP advanced_context
+      | Some '_' -> non_literal_tkn UNDERSCORE advanced_context
+      | Some '*' -> non_literal_tkn ASTERISK advanced_context
+      | Some '-' -> non_literal_tkn MINUS advanced_context
+      | Some '`' -> non_literal_tkn ACUDE advanced_context
+      | Some '\n' -> non_literal_tkn LINEBREAK (advance_line advanced_context)
+      | Some '\\' -> 
+        let new_ctx = (advance advanced_context) in
+        (match current_char new_ctx with 
+            | Some 'n' -> escaped_tkn '\n' new_ctx
+            | Some n -> escaped_tkn n new_ctx
+            | _ -> escaped_tkn ' ' new_ctx)
+      | Some _ -> add_text_literal (set_start advanced_context) 
+      | None -> non_literal_tkn EOF advanced_context
+  
